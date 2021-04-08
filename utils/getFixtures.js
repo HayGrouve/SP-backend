3bd8cb44b69538c9341ccb1f25714edfea7a84b2
Tsvetomir Tsekov <Tsvetomir Tsekov@TKRASIMOVET>
2021-04-08 12:51:31 +0300

    migrating to v3

--- utils/getFixtures.js
+++ utils/getFixtures.js
@@ -22,20 +22,25 @@ const getOddsPerDay = async (days = 0) => {
     let year = currentDate.getFullYear();
     let options = {
       method: 'GET',
-      url: `https://api-football-v1.p.rapidapi.com/v2/odds/date/${year}-${
-        month.toString().length === 1 ? (month = `0${month}`) : month
-      }-${
-        date.toString().length === 1 ? (date = `0${date}`) : date
-      }/bookmaker/8?page=1`,
-      params: { timezone: 'Europe/Sofia' },
+      url: 'https://api-football-v1.p.rapidapi.com/v3/odds',
+      params: {
+        timezone: 'Europe/Sofia',
+        date: `${year}-${
+          month.toString().length === 1 ? (month = `0${month}`) : month
+        }-${date.toString().length === 1 ? (date = `0${date}`) : date}`,
+        page: '1',
+        bookmaker: '8',
+        bet: '1',
+      },
       headers: {
         'x-rapidapi-key': '8c8198bc3fmsh1b23c461c94ca05p19e3c9jsn07c842ff0389',
         'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
       },
     };
     let response = await axios.request(options);
-    let pageTotal = response.data.api.paging.total;
-    await getOddsPerPage(pageTotal, date, month, year);
+    let pageTotal = response.data.paging.total;
+    console.log(pageTotal);
+    // await getOddsPerPage(pageTotal, date, month, year);
   }
   console.log(leagueIdSet);
 };
@@ -44,19 +49,23 @@ const getOddsPerPage = async (totalPages = 1, date, month, year) => {
   for (let i = 1; i <= totalPages; i++) {
     let options = {
       method: 'GET',
-      url: `https://api-football-v1.p.rapidapi.com/v2/odds/date/${year}-${
-        month.toString().length === 1 ? (month = `0${month}`) : month
-      }-${
-        date.toString().length === 1 ? (date = `0${date}`) : date
-      }/bookmaker/8?page=${i}`,
-      params: { timezone: 'Europe/Sofia' },
+      url: 'https://api-football-v1.p.rapidapi.com/v3/odds',
+      params: {
+        timezone: 'Europe/Sofia',
+        date: `${year}-${
+          month.toString().length === 1 ? (month = `0${month}`) : month
+        }-${date.toString().length === 1 ? (date = `0${date}`) : date}`,
+        page: `${i}`,
+        bookmaker: '8',
+        bet: '1',
+      },
       headers: {
         'x-rapidapi-key': '8c8198bc3fmsh1b23c461c94ca05p19e3c9jsn07c842ff0389',
         'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
       },
     };
     let response = await axios.request(options);
-    const odds = response.data.api.odds;
+    const odds = response.data.odds;
     odds.forEach((el) => {
       leagueIdSet.add(el.fixture.league_id);
     });
